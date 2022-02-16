b489403b8e794977380c88907d703176149cf93b
EngincanErgunGG <77359402+EngincanErgunGG@users.noreply.github.com>
2022-02-17 00:23:52 +0300

    .

---
+++ src/EngincanErgunGG/TimeStopper.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace EngincanErgunGG;
+
+use pocketmine\{
+    plugin\PluginBase,
+    event\Listener,
+    event\player\PlayerJoinEvent,
+    event\entity\EntityLevelChangeEvent, 
+    Server
+};
+
+class TimeStopper extends PluginBase implements Listener {
+
+    function onEnable() {
+        $this->getServer()->getPluginManager()->registerEvents(new class($this) implements Listener {
+            function onJoin(PlayerJoinEvent $event) {
+                $level = $event->getPlayer()->getLevel();
+                $level->setTime(0);
+                $level->stopTime();
+            }
+            
+            function onLevelChange(EntityLevelChangeEvent $event) {
+                if (Server::getInstance()->isLevelLoaded($event->getTarget()->getFolderName())){
+                    Server::getInstance()->loadLevel($event->getTarget()->getFolderName());
+                }
+                $event->getTarget()->setTime(0);
+                $event->getTarget()->stopTime();
+            }
+        }, $this);
+        $this->getLogger()->info("Eklenti aktif edildi.");
+    }
+}
